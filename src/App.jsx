be4c194da8b130bdb9c648cae251e24f3685327d
EBliujus <ev.bliujus@gmail.com>
2023-02-22 15:30:28 +0200

    namu darbai

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.scss';
+import Zuikis from './Components/React-base/Zuikis';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <Zuikis></Zuikis>
+        
+        <Zuikis />
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/React-base/Zuikis.jsx
@@ -0,0 +1,10 @@
+function Zuikis () {
+    
+    return (
+
+        <h1 style={{color: 'pink'}}>Labas, Zuiki!</h1>
+
+    )
+}
+
+export default Zuikis
